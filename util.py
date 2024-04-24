0610aa0622f59e925780dadcd7d9b1a0b6c04bb6
Dmitry Sibiryakov <sibiriakoff2006@yandex.ru>
2024-04-24 23:32:33 +0300

    add util

--- util.py
+++ util.py
@@ -389,5 +389,5 @@ def test_quoting():
 # link =   'https://zakupki.gov.ru/epz/order/extendedsearch/results.html?morphology=on&search-filter=%D0%94%D0%B0%D1%82%D0%B5+%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D1%8F&pageNumber=1&sortDirection=false&recordsPerPage=_50&showLotsInfoHidden=false&sortBy=UPDATE_DATE&fz44=on&fz223=on&af=on&ca=on&pc=on&pa=on&currencyIdGeneral=-1'
 # print(parse.unquote(link))
 
-# for k, v in os.environ.items():
-#     print(f'{k}: {v}')
+for k, v in os.environ.items():
+    print(f'{k}: {v}')
